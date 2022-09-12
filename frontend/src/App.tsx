76524cfe95e6d3da3ddb08811b6d7f534b563b30
Waldri <waldri.so@gmail.com>
2022-09-13 00:01:06 +0100

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import NotificationButton from './components/NotificationButton'
 import './index.css'
 
 function App() {
-  return(
-    <h1>Olá mundo</h1>
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <NotificationButton></NotificationButton>
+    </>
   )
 }
 
