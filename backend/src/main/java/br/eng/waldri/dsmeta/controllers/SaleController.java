55478f4662623025c138dbe0355a26ac8b895183
Waldri <waldri.so@gmail.com>
2022-09-26 22:19:11 +0100

    date select

--- backend/src/main/java/br/eng/waldri/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/br/eng/waldri/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package br.eng.waldri.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.eng.waldri.dsmeta.entities.Sale;
@@ -18,7 +19,12 @@ public class SaleController {
 	private SaleService service;
 	
 	@GetMapping
-	public List<Sale> fundSale(){
-		return service.findSale();
+	public Page<Sale> findSale(
+			@RequestParam(value="minDate", defaultValue = "")
+			String minDate, 
+			@RequestParam(value="maxDate", defaultValue = "")
+			String maxDate, 
+			Pageable pageable){
+		return service.findSale(minDate, maxDate, pageable);
 	}
 }
