d3072242cebd7475b86f6377f2fc7c9a61235a0d
Waldri <waldri.so@gmail.com>
2022-09-26 23:31:49 +0100

    Twilio SMS

---
+++ backend/src/main/java/br/eng/waldri/dsmeta/services/SmsService.java
@@ -0,0 +1,45 @@
+package br.eng.waldri.dsmeta.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+import br.eng.waldri.dsmeta.entities.Sale;
+import br.eng.waldri.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SmsService {
+
+	@Value("${twilio.sid}")
+	private String twilioSid;
+
+	@Value("${twilio.key}")
+	private String twilioKey;
+
+	@Value("${twilio.phone.from}")
+	private String twilioPhoneFrom;
+
+	@Value("${twilio.phone.to}")
+	private String twilioPhoneTo;
+	
+	@Autowired
+	private SaleRepository saleRepository;
+	public void sendSms(Long saleId) {
+		
+		Sale sale = saleRepository.findById(saleId).get();
+		String msg = "WaldriAPP: Vendedor " + sale.getSellerName();
+		
+		Twilio.init(twilioSid, twilioKey);
+
+		PhoneNumber to = new PhoneNumber(twilioPhoneTo);
+		PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
+
+		Message message = Message.creator(to, from, msg).create();
+
+		System.out.println(message.getSid());
+	}
+}
